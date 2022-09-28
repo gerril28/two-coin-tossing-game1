99c2bb980e8d9708870364a21f60ed809b0eec34
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-28 19:04:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    if ((0 as any) == ("Heads" as any)) {
-    	
+    if (Coin_1 == "Heads" && Coin_2 == "Heads" || Coin_1 == "Heads" && Coin_2 == "Heads") {
+        Player_1 += 1
+        basic.showNumber(Player_1)
+        basic.showIcon(IconNames.EigthNote)
+        basic.showString("Ply 1")
+    } else {
+        Player_2 += 1
+        basic.showIcon(IconNames.QuarterNote)
+        basic.showString("Ply 2")
     }
 })
-let Coin_1 = "Tails"
-let Coin_2 = "Heads"
 let Player_1 = 0
+let Coin_2 = ""
+let Coin_1 = ""
+Coin_1 = "Tails"
+Coin_2 = "Heads"
+Player_1 = 0
 let Player_2 = 0
